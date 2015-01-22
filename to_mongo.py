8ba9759cc2d0ae46e8596f0a8a02cbf3584812b5
Matt H <paktor132@users.noreply.github.com>
2015-01-22 17:50:54 -0500

    Update to_mongo.py

--- to_mongo.py
+++ to_mongo.py
@@ -1,7 +1,13 @@
 #this is a little script to take files in the /home/vagrant/output directory and put their data back into mongo, indexed by url
 from os import listdir
 from os.path import isfile, join
+import csv
 
 mypath = '/home/vagrant/phoenix_output'
 files = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-print files
+
+for file in files:
+    with open(join(mypath,file),"r") as infile:
+        csvreader = csv.reader(infile, delimiter='\\t', quotechar='')
+        for row in csvreader:
+            print row
